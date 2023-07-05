7736015d03e7cc1538dfc7115f6cb261c5e17eb8
Paulina <paula.woznica@gmail.com>
2023-07-05 16:11:40 +0200

    Gotowa strona z rejestracja(css).

--- rejestracja_form.php
+++ rejestracja_form.php
@@ -14,40 +14,42 @@
 <body>
     <div class="tlo">
         <div class="okno">
-            <div class="test_lewy">
-                <div class="okno_gora">
-                    <img src="grafika/logo.png" alt="logo" id="okno_logo">
+            <div class="okno_menu" id="okno_rejestracja">
+                <div class="zakoncz">
+                    <a href="index.html"><img src="grafika/x.png" alt="zakoncz"></a>
                 </div>
-                <div class="okno_lewy">
-                    <div class="okno_img">
-                        <img src="grafika/kot1.png" alt="szary kot" id="okno_lewykot">
-                    </div>
-                    <div class="okno_menu">
-                        <p id="powitanie">      <!--komunikat początkowy-->
-                            Witaj w świecie,w którym nad ludźmi panują koty.<br>
-                            Wybierz drużynę <b class="wyrozniony_tekst">Miejskich Dachowców</b> lub wciel się <br>w 
-                            <b class="wyrozniony_tekst">Rasową Mafię</b>i razem ze swoim gangiem<br> zacznij podbijać świat!
+                <div id="okno_rejestracja_mniejsze">   
+                    <img src="grafika/logo.png" alt="logo" id="rejestracja_logo">
+                    <p id="info_rejestracja">
+                        Stań się jednym z członków najbardziej niebezpiecznych gangów na świecie!<br>
+                        Wybierz swoją drużynę. <b class="antywyrozniony_tekst"><br>*Pamiętaj zrób to rozsądnie, ponieważ nie można <br>
+                        zrzec się z gangu bez konsekwencji.*</b><br>
+                    </p>
+                    
+                    <div id="formularz_rejestracja">
+                        <p> REJESTRACJA</p>
+                        <p id="info_gangi">
+                            <b class="wyrozniony_tekst">MIEJSKIE DACHOWCE</b> - pozostań z boku i zaskocz w najmniej spodziewanym momencie.<br>
+                            Wyróżniasz się wielką siłą i sprytem, a swoją niezależnością imponujesz nie jednej kotce.<br>
+                            Każda mysz drży na Twój widok!<br>
+                            <b class="wyrozniony_tekst">RASOWA MAFIA</b> - postaw na elegancje i otaczaj się w najbardziej
+                            prestiżowym środowisku.<br> Przemierzaj świat w garniturze, ale nie daj się zwieść swojej waleczności.<br>
+                            Łosoś, kawior i pieniądze to coś do czego dążysz!<br>
                         </p>
-                        <div id="logowanie">
-                            <form id="form_logowanie">
-                                <p>LOGOWANIE</p>
-                                <p id="login_haslo">
-                                    login: <input type="text" name="" id="" value="login"><br>
-                                    hasło: <input type="password" name="" id="" value="hasło"><br>
-                                </p>
-                                <button type="submit">Zaloguj się</button>
-                            </form>
-                            <div id="rejestrowanie">
-                                <p>Nie masz jeszcze konta?</p>
-                                <a href="rejestracja_form.php" class="button">Zarejestruj się</a>
-                            </div>
-                        </div>
+                        <form action="" id="form_rejestracja">
+                            <p id="wybor_p">
+                                <input type="radio" name="wybor_gangu" id="wybor_dachowce" value="dachowce"> MIEJSKIE DACHOWCE
+                                <input type="radio" name="wybor_gangu" id="wybor_mafia" value="mafia"> RASOWA MAFIA <br>
+                            </p>
+                            <p>login: <input type="text" name="rejestracja_login" id="rejestracja_login" value="login"></p>
+                            <p>hasło: <input type="password" name="rejestracja_haslo" id="rejestracja_haslo" value="haslo"></p>
+                            <button type="submit">Zarejestruj się</button>
+                        </form>
                     </div>
                 </div>
             </div>
-            <div class="okno_prawy">
-                <img src="grafika/kot2.png" alt="rudy kot" id="okno_prawykot">
-            </div>
+
+
         </div>
     </div>
 </body>
